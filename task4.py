8becbae611358f8c42d314bd2c1ac9c3c4fa0bcd
Your Name <zaadazz66@gmail.com>
2021-05-11 20:33:32 +0300

    first commit

--- task4.py
+++ task4.py
@@ -7,6 +7,7 @@ Created on Thu Apr 29 17:52:11 2021
 import json
 import urllib
 import requests
+
 file=input("Enter File Name : ")
 def  reader (file):
     lst=[]
@@ -17,7 +18,7 @@ def  reader (file):
     return lst
 cities=(reader(file))
 
-api_key=#I added the API In File in Moodel System ##
+api_key=" " #I added the API In File in Moodel System ##
 
 
 
@@ -26,44 +27,81 @@ parms = dict()
 parms['origins']='תל אביב'
 parms['key'] = api_key
 lst=[]
-dict={}
+lstprint=[]
+dic={}
 address=" "
-for city in cities:
-    parms['destinations']=city
-    address=city
-    url= serviceurl + urllib.parse.urlencode(parms)
-    response=requests.get(url).json()
-    row= response['rows']
-    url2="https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s" % (address,api_key)
-    for rows in (row):
-        element=rows['elements']
-    for elements in (element):
-        distance=elements['distance']
-    for distances in (distance):
-        textdist=distance['text'].strip().replace(" ","") ## the distace i need # 
+try :
+    
+    for city in cities:
+        parms['destinations']=city
+        address=city
+        url= serviceurl + urllib.parse.urlencode(parms)
+        response=requests.get(url).json()
+        row= response['rows']
+        url2="https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s" % (address,api_key)
+        for rows in (row):
+            element=rows['elements']
+        for elements in (element):
+            distance=elements['distance']
+        for distances in (distance):
+            textdist=distance['text'].strip().replace(" ","") ## the distace i need # 
+            
+        for elements in (element):
+            duration=elements['duration']
+        for distances in (distance):
+            textdur=duration['text']
+        if('days' in textdur ):### if in days move to hours ## 
+                text=textdur.split('days')
+                add_houers=int(text[0])*24
+                hours2=text[1].split('hours')
+                houers3=int(hours2[0])
+                hours=add_houers+houers3
+                textdur=str(hours)+hours2[1]+" hours" 
+        if('1 day' in textdur ):  ## if it is just one day ##
+                text=textdur.split('day')
+                add_houers=int(text[0])*24
+                hours2=text[1].split('hours')
+                houers3=int(hours2[0])
+                hours=add_houers+houers3
+                textdur=str(hours)+hours2[1]+" hours"  
         
-    for elements in (element):
-        duration=elements['duration']
-    for distances in (distance):
-        textdur=duration['text'] ## the duration i need #
-    response2 = requests.get(url2).json()
-    result= response2['results']
-    for results in (result):
-        geometry=results['geometry']
-    for geometrys in (geometry):
-        bound=geometry['bounds']
-    for bounds in (bound):
-        northeast=bound['northeast']
-    for lats in (northeast):
-        lat=northeast['lat'] ## the distace i need #        
-    for lngs in (northeast):
-        lng=northeast['lng'] ## the distace i need #   
-    need=(textdist,(textdur),(lat,lng))
-    dic={city: need}
-    lst.append(dic)
-print(lst, sep="\n")    
+        response2 = requests.get(url2).json()##second part ##
+        result= response2['results']
+        for results in (result):
+            geometry=results['geometry']
+        for geometrys in (geometry):
+            bound=geometry['bounds']
+        for bounds in (bound):
+            northeast=bound['northeast']
+        for lats in (northeast):
+            lat=northeast['lat'] ## the distace i need #        
+        for lngs in (northeast):
+            lng=northeast['lng'] ## the distace i need #   
+            need=(textdist,(textdur),(lat,lng))
+        dic.update({city: need})## the dictionary ineed to print ##
+        to_print={'Destination':city,'Distance':textdist,'Duration':textdur,'lat':lat,'Lng':lng}
+        lstprint.append(to_print)
 
-        
+
+    print("the dictonary:")
+    print("---------------------------------------------------")
+    print(dic)
+    print("---------------------------------------------------")
+    print("Printed nice looking")
+    for item in lstprint:
+       print("___________________________")
+       print('\n'.join("{}: {}".format(k, v) for k, v in item.items()))
+       print(" ") 
+
+
+except:
+    if api_key==" ":
+        print("I add the API key in the text file in mooodel-add it in line 20 and try again")
+    else:
+        print("Something went wrong")
+
+
+
 
 
     
